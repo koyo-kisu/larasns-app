51093c4fea23837daf758c630371f12e45b00a36
Koyo Kisu <koyokisu@gmail.com>
2020-03-22 01:07:01 +0900

    更新機能追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -33,4 +33,15 @@ class ArticleController extends Controller
     $article->save();
     return redirect()->route('articles.index');
   }
+
+  public function edit(Article $article)
+  {
+    return view('articles.edit', ['article' => $article]);
+  }
+
+  public function update(ArticleRequest $request, Article $article)
+  {
+    $article->fill($request->all())->save();
+    return redirect()->route('articles.index');
+  }
 }
--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,14 @@
 @csrf
 <div class="md-form">
   <label>タイトル</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+  <!-- NULL合体演算子 -->
+  <!-- 式1 ?? 式2 -->
+  <!-- 式1がnullでない場合（更新）は、式1が結果となる
+       式1がnullである場合（投稿）は、式2が結果となる -->
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+  <!-- NULL合体演算子 -->
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
